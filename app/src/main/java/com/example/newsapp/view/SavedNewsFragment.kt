582e9d3d3dda7b37f2f8bc9b0774bf84349067d7
Ali__Shiri <alishiri067@gmail.com>
2021-12-26 06:04:32 -0800

    add saveNews

--- app/src/main/java/com/example/newsapp/view/SavedNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/view/SavedNewsFragment.kt
@@ -11,29 +11,29 @@ import com.example.newsapp.databinding.FragmentNewsArticleBinding
 import com.example.newsapp.databinding.FragmentSavedNewsBinding
 import com.example.newsapp.viewModel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-//
-//@AndroidEntryPoint
-//class SavedNewsFragment : Fragment() {
-//
-//    private var _binding: FragmentSavedNewsBinding? = null
-//    private val binding get() = _binding!!
+
+@AndroidEntryPoint
+class SavedNewsFragment : Fragment() {
+
+    private var _binding: FragmentSavedNewsBinding? = null
+    private val binding get() = _binding!!
 //    private val viewModel: NewsViewModel by viewModels()
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//
-//        _binding = FragmentSavedNewsBinding.inflate(inflater, container, false)
-//
-//        return binding.root
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//
-//
-//    }
-//}
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentSavedNewsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+}
