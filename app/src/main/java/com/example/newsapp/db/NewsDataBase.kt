8cf9899cbe08d758ba796d60a6f8b277279c4977
Ali__Shiri <alishiri067@gmail.com>
2021-12-26 08:16:36 -0800

    add dataBase

--- app/src/main/java/com/example/newsapp/db/NewsDataBase.kt
+++ app/src/main/java/com/example/newsapp/db/NewsDataBase.kt
@@ -3,13 +3,13 @@ package com.example.newsapp.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-//import com.example.newsapp.model.Article
+import com.example.newsapp.model.Article
 import com.example.newsapp.utils.Converters
 
-//@Database(entities = [Article::class], version = 1, exportSchema = false)
-//@TypeConverters(Converters::class)
-//abstract class NewsDataBase : RoomDatabase(){
-//
-//    abstract fun getDao() : NewsDao
-//
-//}
+@Database(entities = [Article::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class NewsDataBase : RoomDatabase(){
+
+    abstract fun getDao() : NewsDao
+
+}
