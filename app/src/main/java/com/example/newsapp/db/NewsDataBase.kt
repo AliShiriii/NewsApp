4f638bd09b0941b5f98826602a177c106243d515
Ali__Shiri <alishiri067@gmail.com>
2021-12-26 05:56:33 -0800

    add articleNewsFragment

--- app/src/main/java/com/example/newsapp/db/NewsDataBase.kt
+++ app/src/main/java/com/example/newsapp/db/NewsDataBase.kt
@@ -3,7 +3,7 @@ package com.example.newsapp.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.newsapp.model.Article
+//import com.example.newsapp.model.Article
 import com.example.newsapp.utils.Converters
 
 //@Database(entities = [Article::class], version = 1, exportSchema = false)
--- app/src/main/java/com/example/newsapp/view/ArticleNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/view/ArticleNewsFragment.kt
@@ -7,32 +7,32 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.newsapp.databinding.FragmentNewsArticleBinding
-import com.example.newsapp.viewModel.NewsViewModel
+//import com.example.newsapp.viewModel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-//
-//@AndroidEntryPoint
-//class ArticleNewsFragment : Fragment() {
-//
-//    private var _binding: FragmentNewsArticleBinding? = null
-//    private val binding get() = _binding!!
-//
+
+@AndroidEntryPoint
+class ArticleNewsFragment : Fragment() {
+
+    private var _binding: FragmentNewsArticleBinding? = null
+    private val binding get() = _binding!!
+
 //    private val viewModel: NewsViewModel by viewModels()
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//
-//        _binding = FragmentNewsArticleBinding.inflate(inflater, container, false)
-//
-//        return binding.root
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//
-//    }
-//
-//}
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentNewsArticleBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
