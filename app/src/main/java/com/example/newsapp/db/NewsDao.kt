f994395e8bda2fd3a1b9aff748a2294cf9bf443d
Ali__Shiri <alishiri067@gmail.com>
2021-12-26 08:11:07 -0800

    add news dao

--- app/src/main/java/com/example/newsapp/db/NewsDao.kt
+++ app/src/main/java/com/example/newsapp/db/NewsDao.kt
@@ -2,19 +2,18 @@ package com.example.newsapp.db
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-//import com.example.newsapp.model.Article
+import com.example.newsapp.model.Article
 
 @Dao
 interface NewsDao {
 
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    suspend fun insertArticle(article: Article): Long
-//
-//    @Query("SELECT * FROM articles ")
-//    suspend fun getAllArticle(): LiveData<List<Article>>
-//
-//    @Delete
-//    suspend fun deleteArticle(article: Article)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticle(article: Article): Long
 
+    @Query("SELECT * FROM articles ")
+    suspend fun getAllArticle(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
 
 }
