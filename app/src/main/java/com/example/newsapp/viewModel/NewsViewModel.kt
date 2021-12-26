afd638820489606dd8fbdbfd22012b90f194d25a
Ali__Shiri <alishiri067@gmail.com>
2021-12-26 06:22:05 -0800

    add data model

--- app/src/main/java/com/example/newsapp/viewModel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewModel/NewsViewModel.kt
@@ -3,10 +3,10 @@ package com.example.newsapp.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.newsapp.model.Article
-import com.example.newsapp.model.NewsResponse
+//import com.example.newsapp.model.Article
+//import com.example.newsapp.model.NewsResponse
 //import com.example.newsapp.repository.NewsRepository
-import com.example.newsapp.utils.Resource
+//import com.example.newsapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
